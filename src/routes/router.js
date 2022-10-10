f58ee17bb653654ba01494dd3163bb9dc08ce84a
pf222jd <pf222jd@student.lnu.se>
2022-10-10 18:40:16 +0200

    refactor: rename variable

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,9 +3,9 @@ import { UserController } from "../controller/UserController.js";
 import { ChatController } from "../controller/ChatController.js";
 import { authRouter } from "./auth.js";
 
-const authController = new UserController();
+const userController = new UserController();
 const chatController = new ChatController();
 
 export const router = Router();
-router.get("/", authController.isAuthenticated, chatController.index);
+router.get("/", userController.isAuthenticated, chatController.index);
 router.use("/auth", authRouter);
