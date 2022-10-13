fca8232e83b9b439e3b19a1e98d16591091a2d03
pf222jd <pf222jd@student.lnu.se>
2022-10-13 13:08:51 +0200

    refactor: move socket handling to module

--- src/server.js
+++ src/server.js
@@ -7,20 +7,14 @@ import { fileURLToPath } from "node:url";
 import session from "express-session";
 import passport from "passport";
 import morgan from "morgan";
-import { Server } from "socket.io";
 import http from "node:http";
-import MessageModel from "./model/Message.js";
-import SessionStore from "./server/SessionStore.js";
-import crypto from "crypto";
+import SocketManager from "./server/SocketManager.js";
 
-const randomId = () => crypto.randomBytes(8).toString("hex");
-
-const messageModel = new MessageModel();
-const appDir = dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(helmet());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+const appDir = dirname(fileURLToPath(import.meta.url));
 app.set("views", join(appDir, "views"));
 app.use(expressLayouts);
 app.set("layout", join(appDir, "views", "layouts", "default"));
@@ -39,137 +33,10 @@ app.use(sessionMiddleware);
 app.use(passport.authenticate("session"));
 app.use("/", router);
 const httpServer = http.createServer(app);
-const io = new Server(httpServer);
-const wrap = (middleware) => (socket, next) =>
-  middleware(socket.request, {}, next);
-io.use(wrap(sessionMiddleware));
-io.use(wrap(passport.initialize()));
-io.use(wrap(passport.session()));
-
-const sessionStore = new SessionStore();
-io.use((socket, next) => {
-  if (!socket.request.user) {
-    return next(new Error("Unauthorized"));
-  }
-  const sessionId = socket.handshake.auth.sessionId;
-  console.log("sessionId", sessionId);
-  if (sessionId) {
-    console.log("grabbing session from store");
-    const session = sessionStore.findSession(sessionId);
-    if (session) {
-      socket.sessionId = sessionId;
-      socket.userId = session.userId;
-      socket.username = socket.request.user.getUsername();
-      return next();
-    }
-  }
-  socket.username = socket.request.user.getUsername();
-  socket.userId = socket.request.user.getId();
-  socket.sessionId = randomId();
-  sessionStore.saveSession(socket.sessionId, {
-    userId: socket.userId,
-    username: socket.username,
-  });
-  next();
-});
-
-io.on("connection", async (socket) => {
-  socket.emit("session", {
-    sessionId: socket.sessionId,
-    userId: socket.userId,
-    username: socket.username,
-  });
-
-  console.log(socket.username, "connected!");
-  const numberOfUsers = io.of("/").sockets.size;
-  console.log(numberOfUsers, " user(s) connected.");
-
-  console.log(
-    socket.username,
-    "joining socket",
-    socket.userId,
-    "session",
-    socket.sessionId
-  );
-  socket.join(socket.userId.toString());
-
-  const matchingSockets = await io.in(socket.userId.toString()).allSockets();
-  console.log(matchingSockets);
-  if (matchingSockets.size < 2) {
-    // Tell other users that we've connected
-    socket.broadcast.emit("user connected", {
-      socketId: socket.socketId,
-      userId: socket.userId,
-      username: socket.username,
-    });
-  }
-
-  // Get list of all active users
-  const users = [];
-  for (let [id, s] of io.of("/").sockets) {
-    let userExists = false;
-    users.forEach((user) => {
-      if (user.userId == s.userId) {
-        userExists = true;
-      }
-    });
-    if (userExists) {
-      continue;
-    }
-    if (s.userId == socket.userId) {
-      continue;
-    }
-    users.push({
-      socketId: s.socketId,
-      userId: s.userId,
-      username: s.username,
-    });
-  }
-  socket.emit("users", users);
-
-  socket.on("private message", async ({ content, to }) => {
-    console.log("private messages on server", to, content);
-    const newMessage = await messageModel.createMessage(
-      socket.userId,
-      to,
-      content
-    );
-    socket
-      .to(to)
-      .to(socket.userId.toString())
-      .emit("private message", {
-        content: newMessage.getContent(),
-        from: { id: socket.userId, username: socket.username },
-        timestamp: newMessage.getTimestamp(),
-      });
-  });
-
-  socket.on("disconnect", async () => {
-    console.log(numberOfUsers, " user(s) connected.");
-    const matchingSockets = await io.in(socket.userId.toString()).allSockets();
-    console.log("matching sockets", matchingSockets);
-    const isDisconnected = matchingSockets.size === 0;
-    if (isDisconnected) {
-      socket.broadcast.emit("user disconnected", socket.userId);
-    }
-  });
-
-  socket.on("get messages", async ({ user1, user2 }) => {
-    console.log("getting messages");
-    const getMessagesResponse = await messageModel.getMessages(user1, user2);
-    const messagesToEmit = [];
-    getMessagesResponse.forEach((message) => {
-      messagesToEmit.push({
-        from: message.getSender(),
-        to: message.getReceiver(),
-        content: message.getContent(),
-        timestamp: message.getTimestamp(),
-      });
-    });
-    console.log(messagesToEmit);
-    socket.emit("messages", messagesToEmit);
-  });
-});
+const socketManager = new SocketManager(httpServer);
+socketManager.addSessionMiddleware(sessionMiddleware);
+socketManager.addPassportAuth(passport);
+socketManager.run();
 
 httpServer.listen(3000, () => {
   console.log("Listening on *:3000");
---
+++ src/server/SocketManager.js
@@ -0,0 +1,170 @@
+import MessageModel from "../model/Message.js";
+import SessionStore from "./SessionStore.js";
+import crypto from "crypto";
+import { Server } from "socket.io";
+
+export default class SocketManager {
+  #io;
+  #sessionStore = new SessionStore();
+  #messageModel = new MessageModel();
+
+  constructor(httpServer) {
+    this.#io = new Server(httpServer);
+  }
+
+  run = () => {
+    this.#parseSession();
+    this.#handleConnection();
+  };
+
+  addSessionMiddleware(sessionMiddleware) {
+    this.#io.use(this.#wrapper(sessionMiddleware));
+  }
+
+  addPassportAuth(passportAuth) {
+    this.#io.use(this.#wrapper(passportAuth.initialize()));
+    this.#io.use(this.#wrapper(passportAuth.session()));
+  }
+
+  #wrapper = (middleware) => (socket, next) =>
+    middleware(socket.request, {}, next);
+
+  #generateRandomId = () => {
+    return crypto.randomBytes(8).toString("hex");
+  };
+
+  #parseSession = () => {
+    this.#io.use((socket, next) => {
+      if (!socket.request.user) {
+        return next(new Error("Unauthorized"));
+      }
+      const sessionId = socket.handshake.auth.sessionId;
+      console.log("sessionId", sessionId);
+      if (sessionId) {
+        console.log("grabbing session from store");
+        const session = this.#sessionStore.findSession(sessionId);
+        if (session) {
+          socket.sessionId = sessionId;
+          socket.userId = session.userId;
+          socket.username = socket.request.user.getUsername();
+          return next();
+        }
+      }
+      socket.username = socket.request.user.getUsername();
+      socket.userId = socket.request.user.getId();
+      socket.sessionId = this.#generateRandomId();
+      this.#sessionStore.saveSession(socket.sessionId, {
+        userId: socket.userId,
+        username: socket.username,
+      });
+      next();
+    });
+  };
+
+  #handleConnection = () => {
+    this.#io.on("connection", async (socket) => {
+      socket.emit("session", {
+        sessionId: socket.sessionId,
+        userId: socket.userId,
+        username: socket.username,
+      });
+
+      console.log(socket.username, "connected!");
+      const numberOfUsers = this.#io.of("/").sockets.size;
+      console.log(numberOfUsers, " user(s) connected.");
+
+      console.log(
+        socket.username,
+        "joining socket",
+        socket.userId,
+        "session",
+        socket.sessionId
+      );
+      socket.join(socket.userId.toString());
+
+      const matchingSockets = await this.#io
+        .in(socket.userId.toString())
+        .allSockets();
+      console.log(matchingSockets);
+      if (matchingSockets.size < 2) {
+        // Tell other users that we've connected
+        socket.broadcast.emit("user connected", {
+          socketId: socket.socketId,
+          userId: socket.userId,
+          username: socket.username,
+        });
+      }
+
+      // Get list of all active users
+      const users = [];
+      for (let [id, s] of this.#io.of("/").sockets) {
+        let userExists = false;
+        users.forEach((user) => {
+          if (user.userId == s.userId) {
+            userExists = true;
+          }
+        });
+        if (userExists) {
+          continue;
+        }
+        if (s.userId == socket.userId) {
+          continue;
+        }
+        users.push({
+          socketId: s.socketId,
+          userId: s.userId,
+          username: s.username,
+        });
+      }
+      socket.emit("users", users);
+
+      socket.on("private message", async ({ content, to }) => {
+        console.log("private messages on server", to, content);
+        const newMessage = await this.#messageModel.createMessage(
+          socket.userId,
+          to,
+          content
+        );
+        socket
+          .to(to)
+          .to(socket.userId.toString())
+          .emit("private message", {
+            content: newMessage.getContent(),
+            from: { id: socket.userId, username: socket.username },
+            timestamp: newMessage.getTimestamp(),
+          });
+      });
+
+      socket.on("disconnect", async () => {
+        console.log(numberOfUsers, " user(s) connected.");
+        const matchingSockets = await this.#io
+          .in(socket.userId.toString())
+          .allSockets();
+        console.log("matching sockets", matchingSockets);
+        const isDisconnected = matchingSockets.size === 0;
+        if (isDisconnected) {
+          socket.broadcast.emit("user disconnected", socket.userId);
+        }
+      });
+
+      socket.on("get messages", async ({ user1, user2 }) => {
+        console.log("getting messages");
+        const getMessagesResponse = await this.#messageModel.getMessages(
+          user1,
+          user2
+        );
+        const messagesToEmit = [];
+        getMessagesResponse.forEach((message) => {
+          messagesToEmit.push({
+            from: message.getSender(),
+            to: message.getReceiver(),
+            content: message.getContent(),
+            timestamp: message.getTimestamp(),
+          });
+        });
+        console.log(messagesToEmit);
+        socket.emit("messages", messagesToEmit);
+      });
+    });
+  };
+}
