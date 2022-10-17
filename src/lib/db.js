66cd3b01e9c9099514607b25bc0785460640c70c
pf222jd <pf222jd@student.lnu.se>
2022-10-17 09:33:56 +0200

    user env variable for db host

--- src/lib/db.js
+++ src/lib/db.js
@@ -7,7 +7,7 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   user: "postgres",
-  host: "127.0.0.1",
+  host: process.env.DATABASE_URL,
   database: process.env.DATABASE_NAME,
   password: process.env.DATABASE_PASSWORD,
   port: 5432,
