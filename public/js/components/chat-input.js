1175e430e8abd180ebe83a0165c5fbd595d9c3e9
pf222jd <pf222jd@student.lnu.se>
2022-10-17 07:31:56 +0200

    fix: add min width to submit button

--- public/js/components/chat-input.js
+++ public/js/components/chat-input.js
@@ -1,5 +1,10 @@
 const template = document.createElement("template");
 template.innerHTML = `
+<style>
+  #submitbutton {
+    min-width: 130px;
+  }
+</style>
 <link href="/css/bulma.min.css" rel="stylesheet">
 <link rel="stylesheet" href="/vendor/fontawesome/css/all.css">
 <div class="column is-full">
@@ -8,7 +13,7 @@ template.innerHTML = `
       <p class="control is-expanded">
         <input class="input" type="text" id="chat-input" />
       </p>
-      <p class="control">
+      <p class="control" id="submitbutton">
         <button class="button is-info" type="submit">
           Send
         </button>
